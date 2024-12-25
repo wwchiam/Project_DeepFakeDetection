a5341bd468c7b4abb9e5c7fa6a30542ed597d715
wwchiam <wwchiam@gmail.com>
2024-12-26 04:00:18 +0800

    Update with Content Writing

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,25 +138,26 @@ def main():
         return
 
     # Tab Layout
-    tabs = st.tabs(["About", "Detection","Contact Us"])
+    tabs = st.tabs(["About", "Detection","Deep Neural Network", "Contact Us"])
     
     # About Tab
     with tabs[0]:
-        st.subheader("Objective")
-        st.write("Still thinking what to write... ")
+        st.subheader("Detect Deepfakes Instantly")
+        st.write("In an age where manipulated media is becoming alarmingly common, our Deepfake Detection platform empowers users to verify the authenticity of images with just a simple upload. This tool is designed to safeguard public trust, prevent misinformation, and protect against the malicious use of deepfake technology on social media.")
         
-        st.subheader("Who Built This?")
-        st.write("Still thinking what to write... ")
-        
-        st.subheader("Based On")
-        st.write("Still thinking what to write... ")
-        
-        st.subheader("Training Results")
-        st.write("""
-        - **Accuracy**: XXX on the validation dataset.
-        - **Precision**: XXX%
-        - **Recall**: XXX%
+        st.subheader("Why It Matters:")
+        st.markdown("""
+        - **Over 8 million deepfake attempts flood social media weekly, spreading manipulated content and eroding online integrity. (Taeb & Chi, 2022).**  
+        - **Deepfakes fuel misinformation, pose risks to privacy, and undermine trust in digital content.**
         """)
+        
+        st.subheader("How We Help:")
+        st.markdown("""
+        - **Detect & Verify**: Quickly identify manipulated media using cutting-edge deep learning techniques.  
+        - **Report Deepfakes**: Contribute to combating misinformation by reporting suspicious content directly through the platform.  
+        - **Stay Informed**: Access resources and guides to understand and navigate the challenges of deepfake technology.  
+        """)
+        
 
     # Detection Tab
     with tabs[1]:
@@ -182,10 +183,27 @@ def main():
                             st.error(f"Error during prediction: {e}")
                 else:
                     st.warning("Please upload a valid image.")
-    # Contact us Tab
+
+    # Technology
     with tabs[2]:
+        st.subheader("Cutting-Edge AI for Reliable Detection")
+        st.write("Our deepfake detection engine is built on ResNet50, a state-of-the-art convolutional neural network, fine-tuned for precision and reliability." )
+
+        st.subheader("How it works?")
+        st.markdown("""
+            - **Transfer Learning: Utilizing the power of ImageNet pre-trained ResNet50, our model is tailored for detecting deepfakes with advanced fine-tuning.**  
+            - **Diverse Datasets: Trained on a comprehensive dataset sourced from multiple platforms to enhance generalization and robustness.**
+            - **Performance: Optimized to ensure accurate, fast, and scalable detection to meet real-world challenges.**
+
+        st.subheader("Our Training Results")
+            
+            """)
+    
+    # Contact us Tab
+    with tabs[3]:
         st.subheader("Need Help?")
-        st.write("Email to xxx for more information")
+        st.write("Email to 23054196@siswa.um.edu.my for more information")
+
 
 if __name__ == "__main__":
     main()
