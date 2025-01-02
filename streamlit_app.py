1ba784b1f6d6e84ca3e1a3a229f3bc506e5e4caa
wwchiam <wwchiam@gmail.com>
2025-01-03 01:41:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,7 +240,7 @@ model = load_model("improved_resnet50.keras")  # Load your custom .keras file
 def preprocess_image(uploaded_file, target_size=(224, 224)):
     """Preprocess image from file uploader for prediction."""
     try:
-        image = Image.open(uploaded_file).convert("RGB")  # Open and ensure 3 channels (RGB)
+        image = image.open(uploaded_file).convert("RGB")  # Open and ensure 3 channels (RGB)
         image = image.resize(target_size)  # Resize to match the model's input size
         image_array = img_to_array(image)  # Convert to numpy array
         image_array = image_array / 255.0  # Normalize pixel values to 0-1
